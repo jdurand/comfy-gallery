39141a7a17f7d45b13939ecdf1a2c8c811eb66f5
Zach Hamman <zhamman@lightning2.home>
2013-07-29 23:02:24 -0400

    use joahking's example for comfy-blog for comfy-gallery

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
-Rails.application.routes.draw do
-  
-  namespace :admin, :path => ComfyGallery.config.admin_route_prefix do
-    namespace :gallery do
-      resources :galleries do
-        resources :photos do
-          put :reorder, :on => :collection
-          get :crop,    :on => :member
-        end
-      end
-    end
-  end unless ComfyGallery.config.admin_route_prefix.blank?
+defined?(ComfyGallery::Application) && ComfyGallery::Application.routes.draw do
+  ComfyGallery::Routing.admin
 end
---
+++ lib/comfy_gallery/routing.rb
@@ -0,0 +1,17 @@
+module ComfyGallery::Routing
+
+  def self.admin
+    Rails.application.routes.draw do
+      namespace :admin, :path => ComfyGallery.config.admin_route_prefix do
+        namespace :gallery do
+          resources :galleries do
+            resources :photos do
+              put :reorder, :on => :collection
+              get :crop,    :on => :member
+            end 
+          end
+        end
+      end unless ComfyGallery.config.admin_route_prefix.blank?
+    end
+  end
+end
